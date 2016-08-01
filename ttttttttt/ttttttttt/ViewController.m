89ed97fa85243f0266c482a49b748bc5075aa2a5
SQ <865329347@qq.com>
2016-08-01 11:12:46 +0800

    初始化工程

---
+++ ttttttttt/ttttttttt/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ttttttttt
+//
+//  Created by 沈强 on 16/8/1.
+//  Copyright © 2016年 SQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"fasdfsadfa");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
